de184f75d1987068d90f44c59e2e54f7cca5c9a1
mthirani <mthirani@snaplogic.com>
2018-03-31 14:57:55 -0700

    modified the message in class using annotations
    Signed-off-by: mthirani <mthirani@snaplogic.com>

--- src/TestExample.java
+++ src/TestExample.java
@@ -10,19 +10,19 @@ public class TestExample {
     @TestingAnnotations
     void testA() {
         if (true)
-            throw new RuntimeException ("This test always failed");
+            throw new RuntimeException("This test is always failed");
     }
 
     @TestingAnnotations(enabled = false)
     void testB() {
         if (false)
-            throw new RuntimeException ("This test always passed");
+            throw new RuntimeException("This test is always ignored as enabled is false here");
     }
 
     @TestingAnnotations(enabled = true)
     void testC() {
         if (10 > 1) {
-            // do nothing, this test always passed.
+            // do nothing, this test is always passed.
         }
     }
 
