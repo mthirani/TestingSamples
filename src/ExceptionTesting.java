82da7b3003c74ce37b2ec69fb52b984ec668049c
mthirani <mthirani@snaplogic.com>
2019-05-10 10:15:28 -0700

    test git child

--- src/ExceptionTesting.java
+++ src/ExceptionTesting.java
@@ -18,6 +18,7 @@ public class ExceptionTesting {
             testExceptionInParent();
             System.out.println("No Exception caught in main(): ");
             System.out.println("No Exception2 caught in main(): ");
+            System.out.println("Child Exception  caught in main(): ");
         } catch (SQLException e) {
             Throwable t = Throwables.getRootCause(e);
             System.out.println("Exception caught in main(): " + t);
